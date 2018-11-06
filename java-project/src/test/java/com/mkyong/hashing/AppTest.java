6e43b7db381ec0f30bb45883bde748ef556bc2bb
jogender100 <39196428+jogender100@users.noreply.github.com>
2018-11-06 21:52:36 +0000

    Update AppTest.java
    newTest

--- java-project/src/test/java/com/mkyong/hashing/AppTest.java
+++ java-project/src/test/java/com/mkyong/hashing/AppTest.java
@@ -16,7 +16,7 @@ public class AppTest {
     public void testHex() {
         String expected = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
         Assert.assertEquals(expected, App.sha256hex(INPUT));
-        System.out.println("Hello 10th build ");
+        System.out.println("Hello 11th build ");
     }
 
 }
